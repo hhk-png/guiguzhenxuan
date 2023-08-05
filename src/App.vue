4a99f6af82abedd163aaacd6bf19e95ad9dc9b7e
hhk-png <fangluoabc@163.com>
2023-08-05 10:04:05 +0800

    feat: add axios

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <div>
-    测试
-  </div>
+  <div>测试</div>
 </template>
 
 <script setup lang="ts">
 import axios from '@/utils/request'
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
 // url: mock axios instance
 onMounted(async () => {
-  console.log(await axios({
-    url: '/user/login',
-    method: 'post',
-    data: {
-      username: 'admin',
-      password: '123456'
-    }
-  }))
+  console.log(
+    await axios({
+      url: '/user/login',
+      method: 'post',
+      data: {
+        username: 'admin',
+        password: '123456',
+      },
+    }),
+  )
 })
-
-
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
-import {ElMessage} from 'element-plus'
+import { ElMessage } from 'element-plus'
 
 const Axios = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_API,
-  timeout: 5000
+  timeout: 5000,
 })
 
 Axios.interceptors.request.use((config) => {
@@ -17,16 +17,20 @@ const statusCodeToMessage: Record<number, string> = {
   404: '请求地址错误',
   500: '服务器出现问题',
 }
-Axios.interceptors.response.use((response) => {
-  // 处理返回数据
-  return response.data
-}, (error) => {
-  let message: string = statusCodeToMessage[error.response.status] || '请求地址错误'
-  ElMessage({
-    type: 'error',
-    message
-  })
-  return Promise.reject(error)
-})
+Axios.interceptors.response.use(
+  (response) => {
+    // 处理返回数据
+    return response.data
+  },
+  (error) => {
+    let message: string =
+      statusCodeToMessage[error.response.status] || '请求地址错误'
+    ElMessage({
+      type: 'error',
+      message,
+    })
+    return Promise.reject(error)
+  },
+)
 
 export default Axios
