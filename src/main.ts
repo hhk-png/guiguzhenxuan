a4e105a86517070c95752ec80609059d5fe92847
hhk-png <fangluoabc@163.com>
2024-03-21 19:35:31 +0800

    chore: remove useless comment

--- src/main.ts
+++ src/main.ts
@@ -12,26 +12,19 @@ import 'virtual:svg-icons-register'
 import globalComponents from '@/components/globalComponents/index.ts'
 // 清除默认样式
 import '@/styles/index.scss'
-// 路由
 import router from './router/index'
 // 路由守卫
 import './permission'
-// pinia
 import pinia from './store/index'
 
 
 
 const app = createApp(App)
-// 注册路由
 app.use(router)
-
-
-// 安装element plus 插件
 app.use(ElementPlus, {
   locale: zhCn,
 })
-// 注册全局插件
 app.use(globalComponents)
-// pinia
 app.use(pinia)
+
 app.mount('#app')
